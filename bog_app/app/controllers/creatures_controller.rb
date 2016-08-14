01198359e68863d50e2899c66505504ec6d62ed3
YOLANDA RODRIGUEZ <Array415@gmail.com>
2016-08-13 19:19:08 -0700

    create working no errors

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -3,4 +3,18 @@ class CreaturesController < ApplicationController
   def index
     @creatures = Creature.all
   end
+
+  def show
+    @creature = Creature.find(params[:id])
+  end
+
+  def new
+    @creature = Creature.new
+  end
+
+  def create
+    creature_params = params.require(:creature).permit(:name, :description)
+    @creature = Creature.new(creature_params)
+    return redirect_to creature_path(@creature) if @creature.save
+  end
 end

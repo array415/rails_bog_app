ca8b79bd119752a4ef2362dad43024da3d676a64
YOLANDA RODRIGUEZ <Array415@gmail.com>
2016-08-13 23:03:38 -0700

    ugly nav bar and show links added no errors

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -29,4 +29,10 @@ class CreaturesController < ApplicationController
     redirect_to creature_path(@creature)
   end
 
+  def destroy
+    @creature = Creature.find(params[:id])
+    @creature.destroy
+    redirect_to creatures_path
+  end
+
 end

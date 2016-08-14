beecf782e21bc54be5791be3435934b3a4fc19b4
YOLANDA RODRIGUEZ <Array415@gmail.com>
2016-08-14 14:38:58 -0700

    navbar/buttons/basic validation no errors

--- bog-app/app/controllers/creatures_controller.rb
+++ bog-app/app/controllers/creatures_controller.rb
@@ -1,30 +1,49 @@
 class CreaturesController < ApplicationController
+
+
   def index
     @creatures = Creature.all
   end
 
   def new
-      @creature = Creature.new
+    @creature = Creature.new
+    @creature.errors.messages
   end
 
   def create
     creature_params = params.require(:creature).permit(:name, :description)
     @creature = Creature.new(creature_params)
     return redirect_to creature_path(@creature) if @creature.save
+    render "new"
   end
 
   def show
-    @creature = Creature.find(params[:id])
+    @creature = find(params[:id])
   end
 
   def edit
-    @creature = Creature.find(params[:id])
+    @creature = find(params[:id])
   end
 
   def update
     creature_params = params.require(:creature).permit(:name, :description)
-    @creature = Creature.find(params[:id])
+    @creature = find(params[:id])
     @creature.update_attributes(creature_params)
     redirect_to creature_path(@creature)
   end
+
+  def delete
+    @creature = find(params[:id])
+    @creature.destroy
+    redirect_to creatures_path
+  end
+
+
+  private
+
+  def find(id)
+    Creature.find(id)
+  end
+
+
 end

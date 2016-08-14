a80fb556ff2886b2ed0d6cb57c3cbe9f8c0a62f3
YOLANDA RODRIGUEZ <Array415@gmail.com>
2016-08-14 16:18:34 -0700

    edit added no errors

--- bog-app/app/controllers/creatures_controller.rb
+++ bog-app/app/controllers/creatures_controller.rb
@@ -17,4 +17,15 @@ class CreaturesController < ApplicationController
   def show
     @creature = Creature.find(params[:id])
   end
+
+  def edit
+    @creature = Creature.find(params[:id])
+  end
+
+  def update
+    creature_params = params.require(:creature).permit(:name, :description)
+    @creature = Creature.find(params[:id])
+    @creature.update_attributes(creature_params)
+    redirect_to creature_path(@creature)
+  end
 end
